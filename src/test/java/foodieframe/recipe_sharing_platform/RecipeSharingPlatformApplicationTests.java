da9c90b4f47ab32bd9657f481ece88081ccee9f1
DimuthuAro <dimuthuaro@gmail.com>
2025-03-29 16:56:07 +0530

    init

---
+++ src/test/java/foodieframe/recipe_sharing_platform/RecipeSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package foodieframe.recipe_sharing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
